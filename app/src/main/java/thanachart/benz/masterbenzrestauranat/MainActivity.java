09e0285624118adc41df3d28402ce54a4092d166
masterUNG <benzeagle@yahoo.com>
2016-03-19 11:55:50 +0700

    Delete Value SQLite finish

--- app/src/main/java/thanachart/benz/masterbenzrestauranat/MainActivity.java
+++ app/src/main/java/thanachart/benz/masterbenzrestauranat/MainActivity.java
@@ -1,5 +1,6 @@
 package thanachart.benz.masterbenzrestauranat;
 
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -19,9 +20,19 @@ public class MainActivity extends AppCompatActivity {
         myManage = new MyManage(this);
 
         //Test Add Value
-        testAddValue();
+        //testAddValue();
+
+        //Delete All SQLite
+        deleteAllSQLite();
 
     } //Main Method
+
+    private void deleteAllSQLite() {
+        SQLiteDatabase sqLiteDatabase = openOrCreateDatabase(MyOpenHelper.database_name,
+                MODE_PRIVATE, null);
+        sqLiteDatabase.delete(MyManage.user_table, null, null);
+        sqLiteDatabase.delete(MyManage.food_table, null, null);
+    }
 
     private void testAddValue() {
         myManage.addValue(1, "user", "pass", "name");
